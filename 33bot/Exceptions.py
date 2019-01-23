77bd0dc3bed89fa3b7c0477a4de0154a020665fc
KGB33 <33pigbass@gmail.com>
2019-01-22 19:34:19 -0600

    Added Exceptions

---
+++ 33bot/Exceptions.py
@@ -0,0 +1,13 @@
+class ZerothHeroError(Exception):
+    """
+    Arrays start at Zero, but heroes start at one
+    """
+    pass
+
+
+class TooManyHerosError(Exception):
+    """
+    Just give it time, they'll add more heros eventually.
+    """
+    pass
+
