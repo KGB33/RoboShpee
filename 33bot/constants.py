d1b2f8f6e548c1d95f9ac6b93a11ad01e7c1dfde
KGB33 <33pigbass@gmail.com>
2019-01-22 18:06:52 -0600

    reorganized

---
+++ 33bot/constants.py
@@ -0,0 +1,38 @@
+# message prefix that the bot will respond to
+PREFIX = '!3.'
+
+
+# Toggleable roles on the server
+TOGGLEABLE_ROLES = ['Brawlhalla', 'Civilization', 'Dauntless', 'Destiny2', 'DJ', 'Overwatch', 'SmashBros', ]
+
+
+OVERWATCH_HEROS = {
+                1: "Ana",
+                2: "Bastion",
+                3: "Brigitte",
+                4: "D.va",
+                5: "Doomfist",
+                6: "Genji",
+                7: "Hanzo",
+                8: "Junkrat",
+                9: "Lúcio",
+                10: "McCree",
+                11: "Mei",
+                12: "Mercy",
+                13: "Moira",
+                14: "Orisa",
+                15: "Pharah",
+                16: "Reaper",
+                17: "Reinhardt",
+                18: "Roadhog",
+                19: "Soldier: 76",
+                20: "Sombra",
+                21: "Symmetra",
+                22: "Torbjörn",
+                23: "Tracer",
+                24: "Widowmaker",
+                25: "Winston",
+                26: "Wreaking Ball",
+                27: "Zarya",
+                28: "Zenyatta"
+                }
