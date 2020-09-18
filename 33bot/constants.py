8b57e308ee69b6d44d2dac2c9d6937f05c4f9d87
KGB33 <github@bassingthwaite.org>
2020-09-18 11:13:28 -0600

    ran black

--- 33bot/constants.py
+++ 33bot/constants.py
@@ -1,40 +1,40 @@
 # message prefix that the bot will respond to
-PREFIX = '!3.'
-test_PREFIX = 't.'
+PREFIX = "!3."
+test_PREFIX = "t."
 
 
 OVERWATCH_HEROS = {
-                1: "Ana",
-                2: "Ashe",
-                3: "Baptist",
-                4: "Bastion",
-                5: "Brigitte",
-                6: "D.va",
-                7: "Doomfist",
-                8: "Genji",
-                9: "Hanzo",
-                10: "Junkrat",
-                11: "Lúcio",
-                12: "McCree",
-                13: "Mei",
-                14: "Mercy",
-                15: "Moira",
-                16: "Orisa",
-                17: "Pharah",
-                18: "Reaper",
-                19: "Reinhardt",
-                20: "Roadhog",
-                21: "Soldier: 76",
-                22: "Sombra",
-                23: "Symmetra",
-                24: "Torbjörn",
-                25: "Tracer",
-                26: "Widowmaker",
-                27: "Winston",
-                28: "Wreaking Ball",
-                29: "Zarya",
-                30: "Zenyatta"
-                }
+    1: "Ana",
+    2: "Ashe",
+    3: "Baptist",
+    4: "Bastion",
+    5: "Brigitte",
+    6: "D.va",
+    7: "Doomfist",
+    8: "Genji",
+    9: "Hanzo",
+    10: "Junkrat",
+    11: "Lúcio",
+    12: "McCree",
+    13: "Mei",
+    14: "Mercy",
+    15: "Moira",
+    16: "Orisa",
+    17: "Pharah",
+    18: "Reaper",
+    19: "Reinhardt",
+    20: "Roadhog",
+    21: "Soldier: 76",
+    22: "Sombra",
+    23: "Symmetra",
+    24: "Torbjörn",
+    25: "Tracer",
+    26: "Widowmaker",
+    27: "Winston",
+    28: "Wreaking Ball",
+    29: "Zarya",
+    30: "Zenyatta",
+}
 
 
 SHAXX_QUOTES = [
@@ -56,4 +56,4 @@ SHAXX_QUOTES = [
     "How will you fare against utter chaos?",
     "This isn't a fight. It's a massacre.",
     "I will not expect surrender, Guardian. You have dignity to maintain!",
-    ]
+]
--- 33bot/main.py
+++ 33bot/main.py
@@ -8,15 +8,15 @@ from discord.ext import commands
 from constants import PREFIX, OVERWATCH_HEROS, SHAXX_QUOTES
 
 # get TOKEN
-TOKEN = os.environ['DISCORD_TOKEN']
+TOKEN = os.environ["DISCORD_TOKEN"]
 
 
 # Set up logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler('33bot.log')
+handler = logging.FileHandler("33bot.log")
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -29,7 +29,8 @@ async def on_ready():
     print("Logged in as:")
     print(bot.user.name)
     print(bot.user.id)
-    print('--------------\n\n')
+    print("--------------\n\n")
+
 
 # set up opus to use voice
 # discord.opus.load_opus('libopus-0.x86.dll')
@@ -52,27 +53,41 @@ async def golden_gun(ctx):
         if 0 in owned:
             raise ZerothHeroError
     except ValueError:
-        return await ctx.message.channel.send("{}, arguments must be numeric,"
-                                              " try 'heros' for a list of hero-number pairs"
-                                              .format(ctx.message.author.mention))
+        return await ctx.message.channel.send(
+            "{}, arguments must be numeric,"
+            " try 'heros' for a list of hero-number pairs".format(
+                ctx.message.author.mention
+            )
+        )
     except TooManyHerosError:
-        return await ctx.message.channel.send("{}, No, BOB still isn't a playable hero :(\t"
-                                              "*(if more heros have been added let KGB know to update his list)*"
-                                              .format(ctx.message.author.mention))
+        return await ctx.message.channel.send(
+            "{}, No, BOB still isn't a playable hero :(\t"
+            "*(if more heros have been added let KGB know to update his list)*".format(
+                ctx.message.author.mention
+            )
+        )
     except ZerothHeroError:
-        return await ctx.message.channel.send("{}, There isn't a Zeroth Hero".format(ctx.message.author.mention))
+        return await ctx.message.channel.send(
+            "{}, There isn't a Zeroth Hero".format(ctx.message.author.mention)
+        )
     try:
-        return await ctx.message.channel.send("{}, Your next Golden Gun is for {}!".format(
-                              ctx.message.author.mention,
-                              OVERWATCH_HEROS[random.choice(list((OVERWATCH_HEROS.keys() ^ owned)))]))
+        return await ctx.message.channel.send(
+            "{}, Your next Golden Gun is for {}!".format(
+                ctx.message.author.mention,
+                OVERWATCH_HEROS[random.choice(list((OVERWATCH_HEROS.keys() ^ owned)))],
+            )
+        )
     except IndexError:
-        return await ctx.message.channel.send("{} is a liar!, They dont have every golden gun!"
-                             .format(ctx.message.author.mention))
+        return await ctx.message.channel.send(
+            "{} is a liar!, They dont have every golden gun!".format(
+                ctx.message.author.mention
+            )
+        )
 
 
 @bot.command(pass_context=True)
 async def quote(ctx):
-    quotes_channel = discord.utils.get(bot.get_all_channels(), name='quotes')
+    quotes_channel = discord.utils.get(bot.get_all_channels(), name="quotes")
     quotes = []
     async for q in discord.iterators.HistoryIterator(quotes_channel, 100):
         quotes.append(q)
@@ -91,15 +106,21 @@ async def random_num(ctx):
     try:
         max_num = int(ctx.message.content.split()[1])
     except ValueError:  # Max num isn't an int
-        return await ctx.message.channel.send("{}, '{}' isn't a number dummy"
-                             .format(ctx.message.author.mention, ctx.message.content.split()[1]))
+        return await ctx.message.channel.send(
+            "{}, '{}' isn't a number dummy".format(
+                ctx.message.author.mention, ctx.message.content.split()[1]
+            )
+        )
     try:
         rand_num = random.randint(0, max_num)
     except ValueError:  # Max num is negative
         rand_num = random.randint(max_num, 0)
     finally:
-        return await ctx.message.channel.send('{},\tYour random number (between 0 and {}) is: {}'
-                             .format(ctx.message.author.mention, max_num, rand_num))
+        return await ctx.message.channel.send(
+            "{},\tYour random number (between 0 and {}) is: {}".format(
+                ctx.message.author.mention, max_num, rand_num
+            )
+        )
 
 
 @bot.command(pass_context=True)
@@ -112,18 +133,28 @@ async def roles(ctx):
     toggleable_roles = []
 
     for role in ctx.message.channel.guild.roles:
-        if str(role.color) == '#206694':
-            toggleable_roles += [role, ]
+        if str(role.color) == "#206694":
+            toggleable_roles += [
+                role,
+            ]
     toggleable_roles.sort()
 
     for r in reversed(toggleable_roles):
         if r in current_roles:
-            your_toggleable_roles += ['**' + r.name.strip() + '**', ]
+            your_toggleable_roles += [
+                "**" + r.name.strip() + "**",
+            ]
         else:
-            your_toggleable_roles += [r.name, ]
+            your_toggleable_roles += [
+                r.name,
+            ]
 
-    msg = "Your Current Roles: {}\nToggleable Roles: {}".format(current_roles, your_toggleable_roles)
-    return await ctx.message.channel.send("{}:\n{}".format(ctx.message.author.mention, msg))
+    msg = "Your Current Roles: {}\nToggleable Roles: {}".format(
+        current_roles, your_toggleable_roles
+    )
+    return await ctx.message.channel.send(
+        "{}:\n{}".format(ctx.message.author.mention, msg)
+    )
 
 
 @bot.command(pass_context=True)
@@ -133,13 +164,15 @@ async def spam(ctx):
     ex: spam @player 10
     will mention player 10 times
     """
-    if ctx.message.channel.name.lower() == 'spam':
+    if ctx.message.channel.name.lower() == "spam":
         num = int(ctx.message.content.split()[-1])
         for _ in range(num):
             await ctx.message.channel.send(ctx.message.author.mention)
             await asyncio.sleep(random.randint(0, 120))
     else:
-        return await ctx.message.channel.send("This Command can only be called in the '#spam' channel")
+        return await ctx.message.channel.send(
+            "This Command can only be called in the '#spam' channel"
+        )
 
 
 @bot.command(pass_context=True)
@@ -154,48 +187,65 @@ async def toggle_role(ctx):
     # gets toggleable Roles
     toggleable_roles = []
     for role in ctx.message.channel.guild.roles:
-        if str(role.color) == '#206694':
-            toggleable_roles += [role.name, ]
+        if str(role.color) == "#206694":
+            toggleable_roles += [
+                role.name,
+            ]
 
     try:
         role_to_toggle = ctx.message.content.split()[1]
 
-        if role_to_toggle in toggleable_roles:  # Checks to See if given role is toggleable
-            role_to_toggle = discord.utils.get(ctx.message.channel.guild.roles, name=role_to_toggle)
+        if (
+            role_to_toggle in toggleable_roles
+        ):  # Checks to See if given role is toggleable
+            role_to_toggle = discord.utils.get(
+                ctx.message.channel.guild.roles, name=role_to_toggle
+            )
 
             # Toggle role
             if role_to_toggle in ctx.message.author.roles:  # Removes Role
                 await ctx.message.author.remove_roles(role_to_toggle)
-                await ctx.message.channel.send("{}: Role removed".format(ctx.message.author.mention))
+                await ctx.message.channel.send(
+                    "{}: Role removed".format(ctx.message.author.mention)
+                )
             else:  # Gives Role
                 await ctx.message.author.add_roles(role_to_toggle)
-                await ctx.message.channel.send("{}: Role Added".format(ctx.message.author.mention))
+                await ctx.message.channel.send(
+                    "{}: Role Added".format(ctx.message.author.mention)
+                )
 
         # Role is not toggleable
         else:
-            await ctx.message.channel.send('Role Not Found, try "{}roles" for a list of toggleable roles'.format(PREFIX))
+            await ctx.message.channel.send(
+                'Role Not Found, try "{}roles" for a list of toggleable roles'.format(
+                    PREFIX
+                )
+            )
 
     # Checks to see if a role was given
     except IndexError:
-        await ctx.message.channel.send('You must add a valid role, try "{}help" for help'.format(PREFIX))
+        await ctx.message.channel.send(
+            'You must add a valid role, try "{}help" for help'.format(PREFIX)
+        )
 
 
 # Hidden Commands ---------------------------
+
 
 @bot.command(pass_context=True)
 async def hello(ctx):
     """
     Says Hello to whomever called it
     """
-    msg = 'Hi {0.author.mention}'.format(ctx.message)
+    msg = "Hi {0.author.mention}".format(ctx.message)
     await ctx.message.channel.send(msg)
 
 
 @bot.command(pass_context=True)
 async def heros(ctx):
-    msg = ''
+    msg = ""
     for k in OVERWATCH_HEROS:
-        msg += '{}: {}\n'.format(k, OVERWATCH_HEROS[k])
+        msg += "{}: {}\n".format(k, OVERWATCH_HEROS[k])
     await ctx.message.channel.send(msg)
 
 
@@ -217,13 +267,16 @@ async def shaxx(ctx):
         pass
     # Play the shaxx quote
     finally:
-        await ctx.message.channel.send('{}'.format(random.choice(SHAXX_QUOTES)))
+        await ctx.message.channel.send("{}".format(random.choice(SHAXX_QUOTES)))
         await vc.disconnect()
 
 
 @bot.command(pass_context=True)
 async def thanks(ctx):
-    await ctx.message.channel.send("You're Welcome {}!".format(ctx.message.author.mention))
+    await ctx.message.channel.send(
+        "You're Welcome {}!".format(ctx.message.author.mention)
+    )
+
 
 if __name__ == "__main__":
     bot.run(TOKEN)
