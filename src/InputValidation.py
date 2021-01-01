617c811c027a1f88f658a1633fea2547b7e35413
KGB33 <github@bassingthwaite.org>
2021-01-01 04:29:06 -0700

    spilt logic & input validation into seperate files

--- src/InputValidation.py
+++
@@ -1,6 +0,0 @@
-# int i > 0
-def verify_int_g_zero(i):
-    if isinstance(i, int):
-        if i > 0:
-            return True
-    return False
--- src/logic.py
+++ src/logic.py
@@ -1,56 +1,31 @@
+"""
+This file contains the logic behind each
+command defined in `commands.py
+"""
 import random
 
-import discord
-from discord.ext.commands import command
-
-from src import input_validation as inval
-from src import logic
+from src.constants import OW_HEROS, SHAXX_QUOTES
 from src import BASE_DIR, log
-from src.constants import SHAXX_QUOTES, OW_HEROS
 
 
-@command(pass_context=True)
-async def golden_gun(ctx):
-    """
-    Choose the next Overwatch Golden Gun for you to get.
-    For Example:
-            "golden_gun 1 8 15 24 28"
-            Where the numbers are the heros who you already have golden guns
-            for. Try "heros" for hero-number pairs
-    """
-    owned_heros, err = inval.golden_gun(ctx.message.content, ctx.message.author)
-    if err != None:
-        return await ctx.message.channel.send(err)
-    return await ctx.message.channel.send(
-        f"{ctx.message.author.mention} your next golden gun is for {logic.golden_gun(owned_heros)}"
-    )
+def golden_gun(owned: set):
+    return OW_HEROS[random.choice(list((OW_HEROS.keys() ^ owned)))]
 
 
-@command(pass_context=True)
-async def quote(ctx):
-    quotes_channel = discord.utils.get(ctx.guild.channels, name="quotes")
-    quotes = []
-    async for q in discord.iterators.HistoryIterator(quotes_channel, 100):
-        quotes.append(q)
-    return await ctx.message.channel.send(logic.quote(quotes))
+def quote(quotes: list):
+    return random.choice(quotes).content
 
 
-@command(pass_context=True)
-async def random_num(ctx):
+def random_num(lower: int, upper: int) -> str:
     """
     Gets a Random integer between 0 and given max
     For example:
             "random_number 3"
             will return a 0, 1, 2, or 3
     """
-    nums, err = inval.random_num(ctx.message.content)
-    if err != None:
-        return await ctx.message.channel.send(err)
-    lower, upper = nums
-    return await ctx.message.channel.send(logic.random_num(lower, upper))
+    return f"Your random number (between {lower} and {upper}) is: {random.randint(lower, upper)}"
 
 
-@command(pass_context=True)
 async def roles(ctx):
     """
     Displays Your current roles as well as the currently toggleable roles
@@ -84,7 +59,6 @@ async def roles(ctx):
     )
 
 
-@command(pass_context=True)
 async def toggle_role(ctx):
     """
     Toggles given role on or off
@@ -121,12 +95,12 @@ async def toggle_role(ctx):
     except IndexError:
         await ctx.message.channel.send("Could not parse roles")
     except KeyError:
-        keyErrMsg = f'Role Not Found, try "{ctx.bot.command_prefix}roles" for a list of toggleable roles, be sure to check your spelling too.'
+        keyErrMsg = f'Role Not Found, try "{ctx.bot.command_prefix}roles" for '
+        +"a list of toggleable roles, be sure to check your spelling too."
         log.warn(keyErrMsg)
         await ctx.message.channel.send(keyErrMsg)
 
 
-@command(pass_context=True)
 async def hello(ctx):
     """
     Says Hello to whomever called it
@@ -135,7 +109,6 @@ async def hello(ctx):
     await ctx.message.channel.send(msg)
 
 
-@command(pass_context=True)
 async def heros(ctx):
     msg = ""
     for k in OW_HEROS:
@@ -143,7 +116,6 @@ async def heros(ctx):
     await ctx.message.channel.send(msg)
 
 
-@command(pass_context=True)
 async def shaxx(ctx):
     # join the VC
     vc = None
@@ -160,21 +132,18 @@ async def shaxx(ctx):
             await vc.disconnect()
 
 
-@command(pass_context=True)
 async def thanks(ctx):
     await ctx.message.channel.send(
         "You're Welcome {}!".format(ctx.message.author.mention)
     )
 
 
-@command()
 async def echo(ctx):
     return await ctx.message.channel.send(
         ctx.message.content.removeprefix(f"{ctx.bot.command_prefix}echo")
     )
 
 
-@command()
 async def cs(ctx):
     imgs = [
         "bella_cs_chart.png",
