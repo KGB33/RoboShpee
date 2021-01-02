5a8324743ac919943f20ca6198d1ce8e2844b27d
KGB33 <github@bassingthwaite.org>
2021-01-02 02:54:04 -0700

    split up logic around roles and toggle_roles

--- src/logic.py
+++ src/logic.py
@@ -2,6 +2,8 @@
 This file contains the logic behind each
 command defined in `commands.py
 """
+from prettytable import PrettyTable
+
 import random
 
 from src.constants import OW_HEROS, SHAXX_QUOTES
@@ -26,95 +28,40 @@ def random_num(lower: int, upper: int) -> str:
     return f"Your random number (between {lower} and {upper}) is: {random.randint(lower, upper)}"
 
 
-async def roles(ctx):
+def roles(message):
     """
     Displays Your current roles as well as the currently toggleable roles
     """
-    current_roles = [r.name for r in reversed(ctx.message.author.roles)][:-1]
-    your_toggleable_roles = []
-    toggleable_roles = []
-
-    for role in ctx.message.channel.guild.roles:
-        if str(role.color) == "#206694":
-            toggleable_roles += [
-                role,
-            ]
-    toggleable_roles.sort()
-
-    for r in reversed(toggleable_roles):
-        if r in current_roles:
-            your_toggleable_roles += [
-                "**" + r.name.strip() + "**",
-            ]
-        else:
-            your_toggleable_roles += [
-                r.name,
-            ]
-
-    msg = "Your Current Roles: {}\nToggleable Roles: {}".format(
-        current_roles, your_toggleable_roles
-    )
-    return await ctx.message.channel.send(
-        "{}:\n{}".format(ctx.message.author.mention, msg)
-    )
-
-
-async def toggle_role(ctx):
-    """
-    Toggles given role on or off
-    for example:
-            "toggle_role overwatch"
-            will add or remove the overwatch role
-    Try "roles" for Currently Toggleable Roles
-    """
-    # gets toggleable Roles
+    # Generate Data
     toggleable_roles = {
-        role.name.lower(): role
-        for role in ctx.message.channel.guild.roles
-        if str(role.color) == "#206694"
+        # reversed puts the roles in alphabetical order for some reason
+        r: False for r in reversed(message.channel.guild.roles) if str(r.color) == "#206694"
     }
+    for k in toggleable_roles.keys():
+        if k in message.author.roles:
+            toggleable_roles[k] = True
 
-    try:
-        parsed_roles = [r.lower() for r in ctx.message.content.split()][1:]
-        log.info(f"{parsed_roles=}")
-        role_to_toggle = toggleable_roles[parsed_roles[0]]
-        log.info(f"{toggleable_roles=}")
+    # Format Roles to table
+    table = PrettyTable()
+    table.align = "c"
+    table.field_names = ["Toggleable Roles", "Status"]
 
-        # Toggle role
-        if role_to_toggle in ctx.message.author.roles:  # Removes Role
-            await ctx.message.author.remove_roles(role_to_toggle)
-            await ctx.message.channel.send(
-                "{}: Role removed".format(ctx.message.author.mention)
-            )
+    for k, v in toggleable_roles.items():
+        table.add_row([k.name, '✅' if v else ''])
+
+    return "```\n" + table.get_string() + "\n```"
+
+
+async def toggle_role(author, roles: list):
+    out = f"{author.mention}:\n"
+    for r in roles:
+        if r in author.roles:  # Removes Role
+            out += f"\t{r.name} removed\n"
+            await author.remove_roles(r)
         else:  # Gives Role
-            await ctx.message.author.add_roles(role_to_toggle)
-            await ctx.message.channel.send(
-                "{}: Role Added".format(ctx.message.author.mention)
-            )
-
-    except IndexError:
-        await ctx.message.channel.send("Could not parse roles")
-    except KeyError:
-        keyErrMsg = f'Role Not Found, try "{ctx.bot.command_prefix}roles" for '
-        +"a list of toggleable roles, be sure to check your spelling too."
-        log.warn(keyErrMsg)
-        await ctx.message.channel.send(keyErrMsg)
-
-
-async def hello(ctx):
-    """
-    Says Hello to whomever called it
-    """
-    msg = "Hi {0.author.mention}".format(ctx.message)
-    await ctx.message.channel.send(msg)
-
-
-async def heros(ctx):
-    msg = ""
-    for k in OW_HEROS:
-        msg += "{}: {}\n".format(k, OW_HEROS[k])
-    await ctx.message.channel.send(msg)
-
+            out += f"\t{r.name} added\n"
+            await author.add_roles(r)
+    return out
 
 async def shaxx(ctx):
     # join the VC
@@ -132,23 +79,10 @@ async def shaxx(ctx):
             await vc.disconnect()
 
 
-async def thanks(ctx):
-    await ctx.message.channel.send(
-        "You're Welcome {}!".format(ctx.message.author.mention)
-    )
-
-
-async def echo(ctx):
-    return await ctx.message.channel.send(
-        ctx.message.content.removeprefix(f"{ctx.bot.command_prefix}echo")
-    )
-
-
-async def cs(ctx):
+def cs() -> os.Pathlib:
     imgs = [
         "bella_cs_chart.png",
         "magnizar_cs_chart.png",
     ]
     dir_ = BASE_DIR / "static" / random.choice(imgs)
-    log.info(f"`cs` was called, {dir_} returned")
-    return await ctx.message.channel.send(file=discord.File(dir_))
+    return dir_
