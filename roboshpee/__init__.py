509ba54273ae114819797e1ffaf99eb3ba829f3c
KGB33 <github@bassingthwaite.org>
2021-04-04 17:12:52 -0600

    commented out file logging

--- roboshpee/__init__.py
+++ roboshpee/__init__.py
@@ -16,13 +16,15 @@ formatter = logging.Formatter("\n\n%(asctime)s - %(levelname)s - \n\t%(message)s
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.INFO)
 stdout_handler.setFormatter(formatter)
+log.addHandler(stdout_handler)
+"""
 file_handler = logging.FileHandler(
     BASE_DIR / "logs" / f"{datetime.now()}_roboshpee.log"
 )
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
-log.addHandler(stdout_handler)
 log.addHandler(file_handler)
+"""
 
 
 def bot_factory(prefix=PREFIX):
