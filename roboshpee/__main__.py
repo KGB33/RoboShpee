e5d2b375b0550ab76d787c48cbb85b677dc3d8be
KGB33 <github@bassingthwaite.org>
2021-04-05 20:43:54 -0600

    made project structure mirror python-discord's setup

--- roboshpee/__main__.py
+++ roboshpee/__main__.py
@@ -1,11 +1,10 @@
 import os
 
-from . import bot_factory, log
+from roboshpee import bot, log
 
 
 if TOKEN := os.getenv("DISCORD_TOKEN"):
     log.info("Starting Bot.")
-    bot = bot_factory()
     try:
         bot.run(TOKEN)
     except Exception as e:
