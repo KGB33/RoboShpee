eb111bb56d236578a33b4a2643b6321ec0b473da
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2021-07-25 08:36:18 -0700

    Added caching to the file system call.
    Also fixed some typing and linting errors.

--- roboshpee/exts/quine.py
+++ roboshpee/exts/quine.py
@@ -1,6 +1,6 @@
-from typing import Optional
 import os
 from pathlib import Path
+from functools import cache
 
 import discord
 from discord.ext import commands
@@ -12,14 +12,14 @@ from roboshpee.bot import Bot
 
 
 @commands.command()
-async def quine(ctx, file_name: Optional[str] = "quine.py"):
+async def quine(ctx, file_name: str = "quine.py"):
     VALID_FILES = _generate_valid_files()
-    file_name, ratio = process.extractOne(file_name, VALID_FILES.keys())
-    if ratio < 75:
+    file_match = process.extractOne(file_name, VALID_FILES.keys(), score_cutoff=75)
+    if file_match is None:
         return await ctx.send(
-            f"Closest file name is `{file_name}` ({ratio}% match)please try again"
+            f"Could not find the provided file name. Please try again."
         )
-    file_path = VALID_FILES[file_name]
+    file_path = VALID_FILES[file_match[0]]
     with open(file_path) as file_:
         return await ctx.send(file=discord.File(file_))
 
@@ -38,9 +38,10 @@ async def quineables(ctx) -> None:
     return await ctx.send(f"```\n{table.get_string()}\n```")
 
 
-def _generate_valid_files():
+@cache
+def _generate_valid_files() -> dict[str, Path]:
     result = dict()
-    for root, dirs, files in os.walk(BASE_DIR):
+    for root, _, files in os.walk(BASE_DIR):
         for f in files:
             if f.endswith(".py"):
                 result.update({f: Path(root) / f})
