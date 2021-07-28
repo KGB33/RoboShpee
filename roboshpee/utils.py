b672cce6024ba00f496faa6de057806422ea39ff
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2021-07-28 09:12:35 -0700

    Added a helper function to send a PM to the owner.

--- roboshpee/utils.py
+++ roboshpee/utils.py
@@ -2,6 +2,14 @@ import datetime
 import functools
 
 
+async def msg_owner(ctx, message: str):
+    if ctx.guild.owner is None:
+        owner = await ctx.guild.fetch_member(ctx.guild.owner_id)
+    else:
+        owner = ctx.guild.owner
+    await owner.send(message)
+
+
 def ttl_cache(ttl=datetime.timedelta(minutes=3)):
     def wrap(func):
         cache = {}
