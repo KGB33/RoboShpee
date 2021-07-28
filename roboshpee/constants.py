74f61b7670a020e9c25e493ceafcc3e97b61f933
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2021-07-27 18:16:39 -0700

    Added a command to vote on the creation of new roles

--- roboshpee/constants.py
+++ roboshpee/constants.py
@@ -1,8 +1,14 @@
 import os
 from pathlib import Path
+from typing import Final
 
-BASE_DIR = Path(__file__).parent
-PREFIX = os.getenv("BOT_PREFIX", "!3.")
+BASE_DIR: Final = Path(__file__).parent
+PREFIX: Final = os.getenv("BOT_PREFIX", "!3.")
 
 # Logging Trace Level
-TRACE_LEVEL = 5
+TRACE_LEVEL: Final = 5
+
+# Role IDs
+MASTER_SHPEE: Final = 279665426718392320
+ELDER_SHPEE: Final = 757592645185962074
+SENOR_SHPEE: Final = 791704460308119563
