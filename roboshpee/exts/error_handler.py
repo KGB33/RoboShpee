fad1291500d36f23bc683db47661c9a70640feab
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2022-03-21 09:23:41 -0700

    added a simple error message to prevent silent fails

---
+++ roboshpee/exts/error_handler.py
@@ -0,0 +1,64 @@
+import sys
+import traceback
+
+import discord
+from discord.ext import commands
+from discord.ext.commands.help import HelpCommand
+
+from roboshpee.bot import Bot
+from roboshpee.constants import PREFIX
+
+
+class CommandErrorHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        # This prevents any commands with local handlers being handled here in on_command_error.
+        if hasattr(ctx.command, "on_error"):
+            return
+
+        # This prevents any cogs with an overwritten cog_command_error being handled here.
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
+
+        ignored = (commands.CommandNotFound,)
+
+        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
+        # If nothing is found. We keep the exception passed to on_command_error.
+        error = getattr(error, "original", error)
+
+        # Anything in ignored will return and prevent anything happening.
+        if isinstance(error, ignored):
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            await ctx.send(f"{ctx.command} has been disabled.")
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send(
+                    f"{ctx.command} can not be used in Private Messages."
+                )
+            except discord.HTTPException:
+                pass
+        else:
+            # All other Errors not returned come here. And we can just print the default TraceBack.
+            print(
+                "Ignoring exception in command {}:".format(ctx.command), file=sys.stderr
+            )
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr
+            )
+            await ctx.send(
+                f"ERROR: `{error}`\nSee `{PREFIX}help {ctx.command.name}` for more info."
+            )
+
+
+def setup(bot: Bot):
+    bot.add_cog(CommandErrorHandler(bot))
