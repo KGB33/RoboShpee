5bc8b3667db6b94056291782d45ef7922e737eb4
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2022-11-12 10:41:14 -0800

    Cleaned up pipeline flow, added args.

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
+import argparse
 import asyncio
 import os
+import shutil
 import subprocess
+import sys
 
 import dagger
 from dagger.api.gen import Container, ContainerID, Platform
@@ -12,50 +15,55 @@ PLATFORMS: list[Platform] = [
 ]
 
 
-async def build():
+async def main(args: argparse.Namespace):
+    async with dagger.Connection() as client:
+        images = await build(client)
+        if args.publish:
+            await publish(client.container(), images)
+        if not args.skip_tests:
+            print("Tests WIP, skipping anyways.")
+
+
+async def build(client: dagger.Client) -> list[ContainerID]:
     """
     Build a `python:alpine` image packaging roboshpee.
     """
-    async with dagger.Connection() as client:
-        # Grab Lock Files
-        requirment_files = (
-            await client.host().workdir(include=["pyproject.toml", "poetry.lock"]).id()
-        )
+    # Grab Lock Files
+    requirment_files = (
+        await client.host().workdir(include=["pyproject.toml", "poetry.lock"]).id()
+    )
 
-        # Export Poetry lockfile into pip install -r able-format
-        parse_reqs = await (
-            client.container()
-            .from_("alpine:edge")
-            .exec(["apk", "add", "poetry"])
-            .with_mounted_directory("/host", requirment_files)
-            .with_workdir("/host")
-            .exec(["poetry", "export"])
-            .stdout()  # Returns a file
-        ).id()
+    # Export Poetry lockfile into pip install -r able-format
+    parse_reqs = await (
+        client.container()
+        .from_("alpine:edge")
+        .exec(["apk", "add", "poetry"])
+        .with_mounted_directory("/host", requirment_files)
+        .with_workdir("/host")
+        .exec(["poetry", "export"])
+        .stdout()  # Returns a file
+    ).id()
 
-        # Grab Application Source Files
-        source_files = await client.host().workdir(include=["roboshpee/"]).id()
+    # Grab Application Source Files
+    source_files = await client.host().workdir(include=["roboshpee/"]).id()
 
-        # create output directory
-        os.makedirs(f"./build/linux", exist_ok=True)
+    # create output directory
+    os.makedirs(f"./build/linux", exist_ok=True)
 
-        # Build each platform.
-        tasks = []
-        async with asyncio.TaskGroup() as tg:
-            for platform in PLATFORMS:
-                tasks.append(
-                    tg.create_task(
-                        build_platform(client, platform, parse_reqs, source_files)
-                    )
+    # Build each platform.
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        for platform in PLATFORMS:
+            tasks.append(
+                tg.create_task(
+                    build_platform(client, platform, parse_reqs, source_files)
                 )
-
-        await publish(client.container(), [t.result() for t in tasks])
-
-        # Load amd64 image into local docker cache
-        subprocess.run(["docker", "load", "-i", "./build/linux/amd64.tar.gz"])
+            )
+    return [t.result() for t in tasks]
 
 
 async def build_platform(client, platform, requirement_file, src) -> ContainerID:
+    print(f"Building {platform}...")
     ctr = (
         client.container(platform=platform)
         .from_("python:alpine")
@@ -69,12 +77,8 @@ async def build_platform(client, platform, requirement_file, src) -> ContainerID
         .with_entrypoint(["python", "-m", "roboshpee"])
     )
     await ctr.export(f"./build/{platform}.tar.gz")
+    print(f"{platform} done...")
     return await ctr.id()
-
-    # print(f"{platform_variants=}")
-    # await client.container().publish("localhost:5000/roboshpee", platform_variants=platform_variants)
-
-    # Loads the image to run locally. Check stdout for the sha to run.
 
 
 async def test():
@@ -88,10 +92,42 @@ async def publish(ctr: Container, images: list[ContainerID]):
     """
     Push images to ghcr.
     """
+    print("Publishing images to ghcr...")
     await ctr.publish(
         address="ghcr.io/kgb33/roboshpee:latest", platform_variants=images
     )
 
 
+async def load_local():
+    # Load amd64 image into local docker cache
+    subprocess.run(["docker", "load", "-i", "./build/linux/amd64.tar.gz"])
+
+
+def clean():
+    shutil.rmtree("build/")
+
+
 if __name__ == "__main__":
-    asyncio.run(build())
+    parser = argparse.ArgumentParser(
+        description="Builds container images using dagger."
+    )
+    parser.add_argument(
+        "-p", "--publish", action="store_true", help="Publish images to ghcr."
+    )
+    parser.add_argument(
+        "-T", "--skip_tests", action="store_true", help="Don't run tests."
+    )
+    parser.add_argument(
+        "-c",
+        "--clean",
+        action="store_true",
+        help="Removes generated 'build/' directory.",
+    )
+
+    args = parser.parse_args()
+    if args.clean:
+        clean()
+        sys.exit(0)
+
+    print("Starting pipeline...")
+    asyncio.run(main(args))
