b4f5caf23f7f716fb59e37058f503ad12e424090
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2022-11-21 12:26:13 -0800

    Bot now cleans up commands when exiting.

--- roboshpee/__main__.py
+++ roboshpee/__main__.py
@@ -9,19 +9,29 @@ logging_setup()
 log = logging.getLogger(name="bot")
 
 
-def main():
+async def main():
     if TOKEN := os.getenv("DISCORD_TOKEN"):
         log.info("Starting Bot.")
+        instance = Bot.create()
         try:
-            instance = Bot.create()
-            asyncio.run(instance.load_extentions())
-            instance.run(TOKEN)
+            log.info("Logging in...")
+            await instance.login(TOKEN)
+            log.info("Loading Cogs...")
+            await instance.load_extentions()
+            log.info("Connecting...")
+            await instance.connect()
+            log.info("Done!")
         except Exception as e:
             log.error(f"Fatal Exception {e}, exiting now...", exc_info=True)
+        finally:
+            log.info("Clearing Command Tree...")
+            await instance.clear_commands()
+            await instance.close()
+
     elif TOKEN is None:
         log.error("DISCORD_TOKEN environment variable is not set, exiting now...")
         raise EnvironmentError("DISCORD_TOKEN is not set.")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
