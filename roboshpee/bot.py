1ef3d03e23954ef1df8a5d76d209e6d65a61aec0
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2023-12-05 08:52:50 -0800

    feat: Bot automatically converts twitter/x links to nitter. (#15)

--- roboshpee/bot.py
+++ roboshpee/bot.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
+import re
 
-from discord import Intents
+from discord import Intents, Message
 from discord.ext import commands
 
 from roboshpee import constants
+from roboshpee import events
 
 log = logging.getLogger("bot")
 
@@ -22,7 +24,7 @@ class Bot(commands.Bot):
             intents=intents,
             command_prefix=commands.when_mentioned_or(constants.PREFIX),
             *args,
-            **kwargs
+            **kwargs,
         )
 
     async def load_extentions(self) -> None:
@@ -39,6 +41,13 @@ class Bot(commands.Bot):
 
     async def on_connect(self):
         await self.tree.sync()
+
+    async def on_message(self, msg: Message, /) -> None:
+        if (msg.author == self.application_id) or (msg.author.bot):
+            return
+        if match := re.findall(r"https?://(?:twitter|x)\.com/[^.\s]+", msg.content):
+            await events.on_twitter_msg(msg, match)
+        return await super().on_message(msg)
 
     async def clear_commands(self):
         self.tree.clear_commands(guild=None)
