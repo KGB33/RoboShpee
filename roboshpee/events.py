dbeb0cb44892410480eec2a5287d8e904e177448
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-12-05 08:50:48 -0800

    feat: Bot automatically converts twitter/x links to nitter.

---
+++ roboshpee/events.py
@@ -0,0 +1,13 @@
+import re
+
+import discord
+
+
+async def on_twitter_msg(msg: discord.Message, matches: list[str]):
+    reply = "\n".join(
+        re.sub(
+            r"https?://(?:twitter|x)\.com/([^.\s]+)", r"https://nitter.net/\1", match
+        )
+        for match in matches
+    )
+    await msg.reply(reply, mention_author=False)
