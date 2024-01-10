a2c57032ed103be3c7f0ae83dbe64d0a25cd77e3
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2024-01-10 12:30:20 -0800

    Add 'vxtwitter' domain to regex

--- roboshpee/events.py
+++ roboshpee/events.py
@@ -6,7 +6,7 @@ import discord
 async def on_twitter_msg(msg: discord.Message, matches: list[str]):
     reply = "\n".join(
         re.sub(
-            r"https?://(?:twitter|x)\.com/([^.\s]+)", r"https://nitter.net/\1", match
+            r"https?://(?:vxtwitter|twitter|x)\.com/([^.\s]+)", r"https://nitter.net/\1", match
         )
         for match in matches
     )
