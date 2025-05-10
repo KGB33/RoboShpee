2162f52d560e89162e803476068ea7f3fd9a8143
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2025-05-10 14:24:59 -0700

    Inital rust commit

--- flake.nix
+++ flake.nix
@@ -1,26 +1,53 @@
 {
-  description = "Nix baised development environment.";
-
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    dagger = { url = "github:dagger/nix"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = { self, nixpkgs, flake-utils, dagger }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        dag = dagger.packages.${system};
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            dag.dagger
-            pkgs.python311
-            pkgs.just
-          ];
+  outputs = {
+    self,
+    crane,
+    flake-utils,
+    nixpkgs,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
         };
-
-      });
+        rustToolchanfor = p:
+          p.rust-bin.nightly.latest.default.override {
+            targets = ["wasm32-unknown-unknown"];
+          };
+        craneLib =
+          (crane.mkLib pkgs).overrideToolchain rustToolchanfor;
+      in rec
+      {
+        packages = {
+          roboshpee = craneLib.buildPackage {
+            src = craneLib.cleanCargoSource ./.;
+            nativeBuildInputs = with pkgs; [pkg-config openssl];
+          };
+          default = packages.roboshpee;
+        };
+        devShells = {
+          default = craneLib.devShell {
+            inputsFrom = [packages.default];
+            packages = with pkgs; [
+              rust-analyzer
+            ];
+          };
+        };
+      }
+    );
 }
